e865b43b8066e8808c2f64db7fa68a79f1a3e54a
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-09-09 01:32:17 +0530

    initialize data loader

---
+++ readtrackr-data-loader/src/main/java/com/akash/readtrackrdataloader/ReadtrackrDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.akash.readtrackrdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadtrackrDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadtrackrDataLoaderApplication.class, args);
+	}
+
+}
---
+++ readtrackr-data-loader/src/test/java/com/akash/readtrackrdataloader/ReadtrackrDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.readtrackrdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadtrackrDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
