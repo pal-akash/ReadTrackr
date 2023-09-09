9672e06d31c9a82af1e1eeb48af2af144083ccbd
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-09-09 12:37:07 +0530

    set up connection to cassandra

--- readtrackr-data-loader/src/main/java/com/akash/readtrackrdataloader/ReadtrackrDataLoaderApplication.java
+++ readtrackr-data-loader/src/main/java/com/akash/readtrackrdataloader/ReadtrackrDataLoaderApplication.java
@@ -1,13 +1,25 @@
 package com.akash.readtrackrdataloader;
 
+import connection.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class ReadtrackrDataLoaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReadtrackrDataLoaderApplication.class, args);
 	}
 
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+		Path bundle =  astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
 }
