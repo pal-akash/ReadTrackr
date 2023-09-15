e8158005583347e9af770e3219386142b92199af
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-09-16 03:08:32 +0530

    configure authorRepo and main for loading data

--- readtrackr-data-loader/src/main/java/com/akash/readtrackrdataloader/ReadtrackrDataLoaderApplication.java
+++ readtrackr-data-loader/src/main/java/com/akash/readtrackrdataloader/ReadtrackrDataLoaderApplication.java
@@ -1,20 +1,82 @@
 package com.akash.readtrackrdataloader;
 
+import com.akash.readtrackrdataloader.author.Author;
+import com.akash.readtrackrdataloader.author.AuthorRepository;
 import com.akash.readtrackrdataloader.connection.DataStaxAstraProperties;
+import jakarta.annotation.PostConstruct;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class ReadtrackrDataLoaderApplication {
 
+	@Autowired
+	AuthorRepository authorRepository;
+
+	@Value("${datadump.location.author}")
+	private String authorDumpLocation;
+
+	@Value("${datadump.location.works}")
+	private String worksDumpLocation;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ReadtrackrDataLoaderApplication.class, args);
+	}
+
+	private void initAuthors(){
+		Path path = Paths.get(authorDumpLocation);
+		try(Stream<String> lines = Files.lines(path)){
+			lines.forEach(line -> {
+				//Read and parse the line
+
+				String jsonString = line.substring(line.indexOf("{"));
+
+				try {
+					JSONObject jsonObject = new JSONObject(jsonString);
+
+					//Construct the author object
+
+					Author author = new Author();
+					author.setName(jsonObject.optString("name"));
+					author.setPersonalName(jsonObject.optString("personal_name"));
+					author.setId(jsonObject.optString("key").replace("/authors/", ""));
+
+
+					//Persist into the repository
+					System.out.println("Saving author " + author.getName() + "...");
+					authorRepository.save(author);
+				}catch (JSONException e){
+					e.printStackTrace();
+				}
+			});
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	private void initWorks(){
+
+	}
+
+	@PostConstruct
+	public void start(){
+		//System.out.println("Application started");
+		initAuthors();
+		initWorks();
 	}
 
 	@Bean
