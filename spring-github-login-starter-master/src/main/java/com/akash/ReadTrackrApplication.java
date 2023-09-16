f2980c45bb19dfc44a0e9b7ea96ba9d6db097382
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-09-17 01:07:57 +0530

    initialize ReadTrackr

---
+++ spring-github-login-starter-master/src/main/java/com/akash/ReadTrackrApplication.java
@@ -0,0 +1,15 @@
+package com.akash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadTrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadTrackrApplication.class, args);
+	}
+
+	
+
+}
